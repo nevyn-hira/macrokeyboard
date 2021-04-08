ccf9312fb76df6f7b2272abc8a1ba2dcd6a8a232
Nevyn Hira <nevynh@gmail.com>
2021-04-08 19:20:31 +1200

    Move numlock and capslock functionality to own class

---
+++ keyLocks.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: latin-1 -*-
+
+import gi
+import subprocess
+import shlex
+import re
+
+class KeyLocks:
+    def __init__(self):
+        self.disablecaps = False
+        self.forcenum = False
+
+    def execute(self, executable, parameters):
+        if(parameters):
+            subprocess.call([executable] + shlex.split(parameters))
+        else:
+            subprocess.call([executable])
+
+    def DisableCaps(self):
+        self.disablecaps = True
+        if self.IsCaps():
+            self.__ToggleCapsLock()
+
+
+    def ForceNum(self):
+        self.forcenum = True
+        if not self.IsNum():
+            self.__ToggleNumLock()
+
+    def __ToggleNumLock( self ):
+        self.execute('xdotool','key Num_Lock')
+
+    def __ToggleCapsLock( self ):
+        self.execute('xdotool','key Caps_Lock')
+
+    def getXSetOutput(self):
+        return subprocess.getoutput('xset -q')
+
+
+    def IsCaps(self, xsetOutput=""):
+        if xsetOutput=="":
+            out = self.getXSetOutput()
+        else:
+            out = xsetOutput
+        m = re.search("Caps Lock: *(off|on)",out)
+        if m:
+            if m[0].find('on') >= 0:
+                return True
+        return False
+
+    def IsNum(self, xsetOutput=""):
+        if xsetOutput=="":
+            out = self.getXSetOutput()
+        else:
+            out = xsetOutput
+        m = re.search( 'Num Lock: *(off|on)', out )
+        if m:
+            if m[0].find('on') >= 0:
+                return True
+        return False
+            
+    def checkStates(self):
+        out = self.getXSetOutput()
+        print(self.IsCaps(out))
+        if self.disablecaps and self.IsCaps(out):
+            self.__ToggleCapsLock()
+        if self.ForceNum and not self.IsNum(out):
+            self.__ToggleNumLock()
