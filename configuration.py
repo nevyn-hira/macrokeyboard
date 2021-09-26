8c634a061c31495de89d3f4b2092b63525ab2b98
Nevyn Hira <nevynh@gmail.com>
2021-09-27 10:26:47 +1300

    Fix reload functionality

--- configuration.py
+++ configuration.py
@@ -5,6 +5,7 @@ from modifierkeys import Modifierkeys
 
 class Configuration:
     def __init__( self, configFile ):
+        self.keyIndex = {}
         self.config_file = configFile
         self.readConfiguration()
         self.keyIndex = {}
@@ -33,5 +34,12 @@ class Configuration:
         if( not 'device' in self.config ):
             sys.exit('No device defined.')
         else:
+            for ikey in self.config['keys']:
+                keys = ikey.split('+',1)
+                if keys[0] not in self.keyIndex:
+                    self.keyIndex[keys[0]] = []
+                self.keyIndex[keys[0]].append(self.config['keys'][ikey])
+                if len(keys) > 1:
+                    self.keyIndex[keys[0]][len(self.keyIndex[keys[0]]) -1 ]["modifiers"] = keys[1]
             self.device = self.config['device']
             self.modifierkeys = Modifierkeys(self.config['modifier_keys'])
