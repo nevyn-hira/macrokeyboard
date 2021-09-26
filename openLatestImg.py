f27a60c09f78c0251b855d4821ad721ea02dc179
Nevyn Hira <nevynh@gmail.com>
2021-09-27 11:14:45 +1300

    Added initial openLatest functionality.

--- openLatestImg.py
+++ openLatestImg.py
@@ -4,5 +4,10 @@ import glob
 import os
 from Actioner import Actioner
 
-listoffiles = glob.glob(os.path.expanduser('~')+'/Pictures/Screenshot*')
+pictures_folder = os.path.expanduser('~')+'/Pictures/'
+
+listoffiles = glob.glob( pictures_folder + "Screenshot*")
 latest_file = max(listoffiles, key=os.path.getctime)
+actioner = Actioner()
+actioner.gotoLocation(pictures_folder)
+actioner.keySequence('Key.enter|Key.ctrl+l|' + latest_file)
